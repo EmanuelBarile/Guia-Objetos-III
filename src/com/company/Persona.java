2b1913ff7bfec44698a2ed58c22c01d46c88cfd4
unknown <emanuelbarile06@gmail.com>
2021-04-19 20:53:19 -0300

    clase Persona creada en ramaEmanuel

---
+++ src/com/company/Persona.java
@@ -0,0 +1,11 @@
+package com.company;
+
+    public class Persona {
+
+        private String dni;
+        private String nombre;
+        private String apellido;
+        private String email;
+        private String direccion;
+
+}
